3b40f9389cdeb083df3e534052725014441d84a5
SofyaShepelevaIR224 <shepeleva.sofya91@gmail.com>
2024-11-13 14:48:02 +0300

    commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    println("Введите строку:") //Запрашиваем ввод строки у пользователя
+    val input = readLine() ?: "" //Считывание строки которую ввел пользователь
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    val symbol = input.toSet().sorted() //toSet удаляет дубликаты, sorted сорт-т по алфавиту
+
+    val result = StringBuilder("Результат: \n") //Формирует выходную строку
+    var totalCount = 0 //Хранит общее количество символов
+
+    for (char in symbol) { //Начало цикла
+        val count = input.count { it == char } //Подсчет повторений уникальных символов
+        result.append("$char: $count\n") //Помещаем всю информацию в одной строке
+        totalCount += 1 //Подсчет количества уникальных символов
     }
+
+   println(result.toString())
 }
